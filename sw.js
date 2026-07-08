3603ccbb80f7b8b78ea85106d3f6a3d3fdce8cd4
pepealltrades <pepealltrades@gmail.com>
2026-07-08 10:25:02 +0200

    PWA del generador de mocions (versió mòbil)

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE="mocions-11ae8cac";
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){
+    return c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"]);
+  }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){ return self.clients.claim(); }));
+});
+self.addEventListener("fetch",function(e){
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(function(r){ return r||fetch(e.request); }));
+});
